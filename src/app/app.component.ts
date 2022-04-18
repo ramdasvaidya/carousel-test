19a1e1ebd6dcdee4894c119f1424695b531379db
Ramdas Vaidya <rv.ramdasvaidya@gmail.com>
2022-04-18 21:13:10 +0100

    Code commit for get product data from json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carousel-test';
+  productList:any = [];
+
+  // Json data path
+  private _jsonURL = 'assets/json/data.json';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getProductList().subscribe(data => {
+      this.productList = data.carouselData;
+      console.log(this.productList);
+    });
+  }
+
+  public getProductList(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
 }
